6c169b99624e70337fa33932c7dfdfbb539286f8
SydneyEckstein <sydney.eckstein@fsinvestments.com>
2025-09-29 11:47:08 -0400

    Added start button
    Adjusted window size, added buttons to menu scene.

---
+++ DX Ball/Assets/Scripts/menu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public TextMeshProUGUI startButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
