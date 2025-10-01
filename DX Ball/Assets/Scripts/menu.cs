629bbebd73368e539fadef588d27f1b8eee8ac6d
SydneyEckstein <sydney.eckstein@fsinvestments.com>
2025-09-30 21:30:07 -0400

    Implemented start button in menu scene

--- DX Ball/Assets/Scripts/menu.cs
+++ DX Ball/Assets/Scripts/menu.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI; // Required for UI elements
+using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    public TextMeshProUGUI startButton;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene("Level1");
+
     }
 }
